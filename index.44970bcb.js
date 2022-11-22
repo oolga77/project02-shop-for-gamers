c66548e1bcae4734dd5950405fa194f87f659e11
oolga77 <43237340+oolga77@users.noreply.github.com>
2022-11-22 21:41:50 +0000

    Deploying to gh-pages from @ oolga77/project02-shop-for-gamers@62c2e0cf97d535032a99f9886163f0d473d6132e 🚀

---
+++ index.44970bcb.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openMenuBtn:document.querySelector("[data-menu-open]"),menu:document.querySelector("[data-menu]")}).openMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-open"),e.openMenuBtn.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.44970bcb.js.map
