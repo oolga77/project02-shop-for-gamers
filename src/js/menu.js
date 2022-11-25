80a6b59f1113735b457cabf8210323cd93dd188d
Anna Stepanenko <98412801+annastpnka@users.noreply.github.com>
2022-11-25 03:10:25 +0100

    header-pull

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,15 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-   // closeMenuBtn: document.querySelector("[data-menu-btn]"),
-    menu: document.querySelector("[data-menu]"),
-  };
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-btn]'),
+//     menu: document.querySelector('[data-menu]'),
+//   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  // refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-open");
-    refs.openMenuBtn.classList.toggle("is-open");
-  }
-})();
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-open');
+//     refs.openMenuBtn.classList.toggle('is-open');
+//   }
+// })();
