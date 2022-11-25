0cc60d7e0c63e3fa738bf08fe6098807d9f8855d
YaroslavKorniienko <yarikkornej@gmail.com>
2022-11-25 04:35:32 +0200

    fixed mistakes for filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,15 +1,15 @@
 function app() {
-  const buttons = document.querySelectorAll('.button');
-  const cards = document.querySelectorAll('.box');
+  const buttons = document.querySelectorAll('.accessories-button');
+  const cards = document.querySelectorAll('.accessories-item');
 
   function filter(category, items) {
     items.forEach(item => {
       const isItemFiltered = !item.classList.contains(category);
       const isShowAll = category.toLowerCase() === 'all';
       if (isItemFiltered && !isShowAll) {
-        item.classList.add('visually-hidden');
+        item.classList.add('is-hidden');
       } else {
-        item.classList.remove('visually-hidden');
+        item.classList.remove('is-hidden');
       }
     });
   }
