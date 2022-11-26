95afe33ed39a6eb306214e8f01c724938bc0e9f1
oolga77 <43237340+oolga77@users.noreply.github.com>
2022-11-26 18:39:08 +0000

    Deploying to gh-pages from @ oolga77/project02-shop-for-gamers@7280914c69538a3cd4e44ed3ed1d51172f990c41 🚀

---
+++ contacts-page.5a1053ad.js
@@ -0,0 +1,2 @@
+!function(){var e={openSendBtn:document.querySelector("[data-send-open]"),closeSendBtn:document.querySelector("[data-send-close]"),send:document.querySelector("[data-send]")};function n(){e.send.classList.toggle("is-hidden")}e.openSendBtn.addEventListener("click",n),e.closeSendBtn.addEventListener("click",n)}();
+//# sourceMappingURL=contacts-page.5a1053ad.js.map
